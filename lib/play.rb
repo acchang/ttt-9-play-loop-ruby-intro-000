b2feeb8beeee685e5dc876927fe14e1ae3ac1a46
Andrew C <acchang4@gmail.com>
2020-04-10 00:38:13 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,8 +37,9 @@ end
 
 # Define your play method below
 def play(board)
+  turn(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   until board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-  turn(board)
+
 end
 end
